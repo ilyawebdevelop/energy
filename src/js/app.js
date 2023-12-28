c7f70475b173170204514048539680359b38ca8a
ilyawebdevelop <ilia966@tut.by>
2023-12-28 14:39:04 +0300

    28.12.23

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,8 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar } from 'swiper';
 Swiper.use([Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar]);
 
+const mediaQuery575 = window.matchMedia('(max-width: 575px)');
+
 // Инициализация слайдера intro-slider
 const introSlider = document.querySelector('.intro-slider');
 var mySwiperIntro = new Swiper(introSlider, {
@@ -26,7 +28,43 @@ var mySwiperIntro = new Swiper(introSlider, {
   },
 });
 
-// Инициализация слайдера 
+
+if (mediaQuery575.matches) {
+
+  // Инициализация слайдера services-slider
+  document.querySelectorAll('.services').forEach(n => {
+    const mySwiperServices = new Swiper(n.querySelector('.services-slider'), {
+      slidesPerView: 1,
+      speed: 800,
+      spaceBetween: 20,
+      pagination: {
+        el: document.querySelector('.services .swiper-pagination'),
+        clickable: true,
+        type: 'bullets',
+      },
+      on: {
+        slideChange: function () {
+          var slides_count = n.querySelectorAll(".swiper-slide"); // <- add this
+          let offer = document.querySelector('.slider-count');
+          offer.innerHTML = `<span>` + (mySwiperServices.activeIndex + 1) + `</span>` + ` / ` + (slides_count.length);
+        }
+      },
+    });
+
+    function change() {
+
+      setTimeout(function () {
+        var slides_count = n.querySelectorAll(".swiper-slide");
+        let offer_1 = n.querySelector('.slider-count');
+        offer_1.innerHTML = `<span>` + (mySwiperServices.activeIndex + 1) + `</span>` + ` / ` + (slides_count.length);
+      }, 1000);
+    }
+    change();
+  });
+
+}
+
+// Инициализация слайдера projects-slider
 const projectsSlider = document.querySelector('.projects-slider');
 var mySwiperProjects = new Swiper(projectsSlider, {
   slidesPerView: 3,
@@ -39,20 +77,23 @@ var mySwiperProjects = new Swiper(projectsSlider, {
   breakpoints: {
     0: {
       slidesPerView: 1,
-      spaceBetween: 15,
     },
     576: {
       slidesPerView: 2,
-      spaceBetween: 20,
     },
     992: {
       slidesPerView: 3,
-      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+    1400: {
+      slidesPerView: 3,
     },
   },
 });
 
-// Инициализация слайдера 
+// Инициализация слайдера news-slider
 const newsSlider = document.querySelector('.news-slider');
 var mySwiperNews = new Swiper(newsSlider, {
   slidesPerView: 3,
@@ -65,15 +106,12 @@ var mySwiperNews = new Swiper(newsSlider, {
   breakpoints: {
     0: {
       slidesPerView: 1,
-      spaceBetween: 15,
     },
-    576: {
+    768: {
       slidesPerView: 2,
-      spaceBetween: 20,
     },
     992: {
       slidesPerView: 3,
-      spaceBetween: 30,
     },
   },
 });
@@ -91,11 +129,58 @@ var mySwiperProjectsService = new Swiper(projectsSliderService, {
   breakpoints: {
     0: {
       slidesPerView: 1,
-      spaceBetween: 15,
     },
     576: {
       slidesPerView: 2,
-      spaceBetween: 20,
-    },   
+    },
   },
+});
+
+// Burger
+const btnMenu = document.querySelector('#toggle');
+const menu = document.querySelector('.header-mob');
+const searchMob = document.querySelector('.search-mob');
+const menuClose = document.querySelector('.header-mob-close');
+const searchBtnMob = document.querySelector('.header__search-btn-mob');
+const searchBackMob = document.querySelector('.search-mob-back');
+const bodyEl = document.querySelector('body');
+let navItemAll = document.querySelectorAll('.headerNavList li a');
+
+const toggleMenu = function () {
+  menu.classList.toggle('active');
+}
+const toggleBurger = function () {
+  btnMenu.classList.toggle('active');
+}
+const menuCloseF = function () {
+  menu.classList.remove('active');
+  toggleBurger();
+  bodyOverflow();
+}
+const bodyOverflow = function () {
+  bodyEl.classList.toggle('hidden');
+}
+
+btnMenu?.addEventListener('click', function (e) {
+  e.stopPropagation();
+  toggleMenu();
+  toggleBurger();
+  bodyOverflow();
+});
+menuClose?.addEventListener('click', function (e) {
+  menuCloseF();
+});
+searchBtnMob.addEventListener('click', function (e) {
+  searchMob.classList.add('active');
+
+});
+searchBackMob.addEventListener('click', function (e) {
+  searchMob.classList.remove('active');
+});
+
+// close menu in Landing page
+$(document).on("click", ".headerNavList li a", function (e) {
+  $('.header-mob').removeClass('active');
+  $('body').removeClass('hidden');
+  toggleBurger();
 });
